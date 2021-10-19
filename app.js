84e0e0c48f7e75ed0c3171bc2a68a24931963841
Debjit Pal <debjitpal5040@gmail.com>
2021-10-19 20:18:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,101 +1,89 @@
-(function(){
+(function()
+{
     "use strict";
-    var el = function(element){
-    if (element.charAt(0) === "#"){ // If passed an ID...
-        return document.querySelector(element); // ... returns single element
-    }
-    return document.querySelectorAll(element); // Otherwise, returns a nodelist
-    };
-    // Variables
-    var viewer = el("#space"), // Calculator screen where result is displayed
-    equals = el("#equal"), // Equal button
-    nums = el(".button"), // List of numbers
-    ops = el(".operators"), // List of operators
-    theNum = "", // Current number
-    oldNum = "", // First number
-    resultNum, // Result
-    operator; // Batman
-    // When: Number is clicked. Get the current number selected
-    var setNum = function(){
-    if (resultNum){ // If a result was displayed, reset number
-        theNum = this.getAttribute("data-num");
-        resultNum = "";
-    } 
-    else{ // Otherwise, add digit to previous number (this is a string!)
-        theNum += this.getAttribute("data-num");
-    }
-    console.log(theNum+' is clicked')
-    viewer.innerHTML = theNum; // Display current number
-    };
-    // When: Operator is clicked. Pass number to oldNum and save operator
-    var moveNum = function(){
-    oldNum = theNum;
-    theNum = "";
-    operator = this.getAttribute("data-ops");
-    //equals.setAttribute("data-result", ""); // Reset result in attr
-    console.log(operator+' is clicked')
-    };
-    // When: Equals is clicked. Calculate result
-    // ??
-    var displayNum = function(){
-    // Convert string input to numbers
-    oldNum = parseFloat(oldNum);
-    theNum = parseFloat(theNum);
-    // Perform operation
-    switch (operator){
-        case "plus":
-            resultNum = oldNum + theNum;
-            break;
-        case "minus":
-            resultNum = oldNum - theNum;
-            break;
-        case "times":
-            resultNum = oldNum * theNum;
-            break;
-        case "divided by":
-            resultNum = (oldNum / theNum).toFixed(3);
-            break;
-        // If equal is pressed without an operator, keep number and continue
-        default:
-            resultNum = theNum;
-    }
-    // If NaN or Infinity returned
-    if (!isFinite(resultNum)){
-        if (isNaN(resultNum)){ // If result is not a number; set off by, eg, double-clicking operators
-            resultNum = "0";
-        } 
-        else{ // If result is infinity, set off by dividing by zero
-            resultNum = "Infinte";
-            el('#calculator').classList.add("broken"); // Break calculator
-            el('#reset').classList.add("show"); // And show reset button
+    var el = function(element)
+    {
+        if (element.charAt(0) === "#")
+        {
+            return document.querySelector(element);
         }
-    }
-    // Display result, finally!
-    viewer.innerHTML = resultNum;
-    equals.setAttribute("data-result", resultNum);
-    // Now reset oldNum & keep result
-    oldNum = 0;
-    theNum = resultNum;
+        return document.querySelectorAll(element);
     };
-    // When: Clear button is pressed. Clear everything
-    var clearAll = function(){
-    oldNum = "";
-    theNum = "";
-    viewer.innerHTML = "0";
-    equals.setAttribute("data-result", resultNum);
+    var viewer = el("#space"), equals = el("#equal"), nums = el(".button"), ops = el(".operators"), theNum = "", oldNum = "", resultNum, operator;
+    var setNum = function()
+    {
+        if (resultNum)
+        {
+            theNum = this.getAttribute("data-num");
+            resultNum = "";
+        } 
+        else
+        {
+            theNum += this.getAttribute("data-num");
+        }
+        console.log(theNum+' is clicked')
+        viewer.innerHTML = theNum;
     };
-    /* The click events */
-    // Add click event to numbers
-    for (var i = 0, l = nums.length; i < l; i++){
+    var moveNum = function()
+    {
+        oldNum = theNum;
+        theNum = "";
+        operator = this.getAttribute("data-ops");
+        console.log(operator+' is clicked')
+    };
+    var displayNum = function()
+    {
+        oldNum = parseFloat(oldNum);
+        theNum = parseFloat(theNum);
+        switch (operator)
+        {
+            case "plus":
+                resultNum = oldNum + theNum;
+                break;
+            case "minus":
+                resultNum = oldNum - theNum;
+                break;
+            case "times":
+                resultNum = oldNum * theNum;
+                break;
+            case "divided by":
+                resultNum = oldNum / theNum;
+                break;
+            default:
+                resultNum = theNum;
+        }
+        if (!isFinite(resultNum))
+        {
+            if (isNaN(resultNum))
+            {
+                resultNum = "0";
+            } 
+            else
+            {
+                resultNum = "Undefined";
+            }
+        }
+        viewer.innerHTML = resultNum;
+        equals.setAttribute("data-result", resultNum);
+        oldNum = 0;
+        theNum = resultNum;
+    };
+    var clearAll = function()
+    {
+        oldNum = "";
+        theNum = "";
+        viewer.innerHTML = "0";
+        equals.setAttribute("data-result", resultNum);
+    };
+    for (var i = 0, l = nums.length; i < l; i++)
+    {
         nums[i].onclick = setNum;
     }
-    // Add click event to operators
-    for (var i = 0, l = ops.length; i < l; i++){
+    for (var i = 0, l = ops.length; i < l; i++)
+    {
         ops[i].onclick = moveNum;
     }
-    // Add click event to equal sign
     equals.onclick = displayNum;
     equals.onclick(console.log(' =  is clicked'))
-    // Add click event to clear button
     el("#ac").onclick = clearAll;
 }());
